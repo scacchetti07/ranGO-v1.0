80880f80c0ead6721a6bcc46305b42c699977010
Scacchetti <luiscacchetti07@gmail.com>
2025-04-03 14:59:57 -0300

    Adcionando Nova Message box com Progress bar para restaurar dados

---
+++ MarketProject/Controls/RestoringData_MsgBox.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MarketProject.Controls;
+
+public partial class RestoringData_MsgBox : UserControl
+{
+    public RestoringData_MsgBox()
+    {
+        InitializeComponent();
+    }
+}
